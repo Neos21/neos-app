f93c3bcdd51e451f62fc419025bb5523d64bf21b
Neos21 <neos21@gmail.com>
2023-08-11 21:38:16 +0900

    Fix

--- server/src/modules/bookmarks/bookmarks.controller.ts
+++ server/src/modules/bookmarks/bookmarks.controller.ts
@@ -29,7 +29,7 @@ export class BookmarksController {
   
   /** ブックマークレットからの呼び出し用 */
   @Get('add')
-  public async addFromBookmarklet(@Query('credential') credential: string, @Query('url') url: string, @Res() res: Response): Promise<void> {
+  public async addFromBookmarklet(@Query('credential') credential: string, @Query('url') url: string, @Res() res: Response): Promise<void | Response> {
     if(credential !== this.configService.get('password')) return res.status(HttpStatus.UNAUTHORIZED).json({ error: 'Invalid Credential' });
     const insertResult = await this.bookmarksService.create(url);
     if(insertResult == null) return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: 'Failed To Create Bookmark' });
